f6f1c2911d2235ca279da8a0b531d20d7bdc5ffd
Vishal Suryavanshi <iamvishsurya@gmail.com>
2024-01-01 16:57:01 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -7,14 +7,14 @@ function openNav() {
 
 }
 
-// display current year on footer
-function getCurrentYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
+// // display current year on footer
+// function getCurrentYear() {
+//     var currentDate = new Date();
+//     var currentYear = currentDate.getFullYear();
+//     document.querySelector("#displayYear").innerHTML = currentYear;
+// }
 
-getCurrentYear();
+// getCurrentYear();
 
 // carousel indicator style
 var liOne = $(".slider_section .carousel-indicators li.li_one");
